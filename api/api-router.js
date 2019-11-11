46d4376c4572129739caf5b5728c28cb9313e770
Austin Powell <apst12012@gmail.com>
2019-11-11 12:32:33 -0500

    bcrypt test

--- api/api-router.js
+++ api/api-router.js
@@ -2,12 +2,19 @@ const router = require('express').Router();
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const bcrypt = require('bcryptjs');
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post('/hash', (req, res) => {
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+res.json({password: req.body.password, hash: hash})
 });
 
 module.exports = router;
